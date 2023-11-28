f7f680e5fb7d49ce255757b45db2f4f0e74bf432
DominikEichinger <dominik@eichi.org>
2023-11-28 08:40:01 +0100

    progress

---
+++ script.js
@@ -0,0 +1,19 @@
+// TODO: Implement the oddNumbers function:
+//The function has two parameters, both are numbers.
+//Iterate over all numbers between first number and the second number.
+//Store all oddNumbers in string, seperated by a comma and return the string.
+//Allowed are only positive numbers, add a check.
+
+function oddnumbers (a, b) {
+    const isodd !== Number.isInteger(a, b / 2);
+
+}
+
+console.log(oddNumbers(0, 4));
+// result should be: 1,3
+
+console.log(oddNumbers(10, 33));
+// result should be: 11,13,15,17,19,21,23,25,27,29,31,33
+
+console.log(oddNumbers(9, 12));
+// result should be: 9,11
